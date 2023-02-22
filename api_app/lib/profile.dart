3ed1d703be29d106aff90eb5feb98740f2bd8a53
St-Luciferr <santosh.762417@thc.tu.edu.np>
2023-02-23 03:00:53 +0545

    search feature added

--- api_app/lib/profile.dart
+++ api_app/lib/profile.dart
@@ -12,91 +12,158 @@ class UserProfile extends StatefulWidget {
 }
 
 class _UserProfileState extends State<UserProfile> {
+  List<Map<String, dynamic>> _allData = [];
+  List<Map<String, dynamic>> _foundData = [];
+  @override
+  initState() {
+    //creating list for all the user data to show to user
+    _allData = [
+      {
+        "label": "Name: ",
+        "text":
+            "${widget.users[0]['name']['title']}. ${widget.users[0]['name']['first']} ${widget.users[0]['name']['last']}",
+      },
+      {
+        "label": "Username: ",
+        "text": "${widget.users[0]['login']['username']}",
+      },
+      {
+        "label": "Email: ",
+        "text": "${widget.users[0]['email']}",
+      },
+      {
+        "label": "Phone: ",
+        "text": "${widget.users[0]['phone']}",
+      },
+      {
+        "label": "Cell: ",
+        "text": "${widget.users[0]['Cell']}",
+      },
+      {
+        "label": "Gender: ",
+        "text": "${widget.users[0]['Gendor']}",
+      },
+      {
+        "label": "DOB: ",
+        "text": "${widget.users[0]['dob']['date'].substring(0, 10)}",
+      },
+      {
+        "label": "Age: ",
+        "text": "${widget.users[0]['dob']['age']}",
+      },
+      {
+        "label": "Country: ",
+        "text": "${widget.users[0]['location']['country']}",
+      },
+      {
+        "label": "State: ",
+        "text": "${widget.users[0]['location']['state']}",
+      },
+      {
+        "label": "City: ",
+        "text": "${widget.users[0]['location']['city']}",
+      },
+      {
+        "label": "Street: ",
+        "text":
+            "${widget.users[0]['location']['street']['number']}, ${widget.users[0]['location']['street']['name']}",
+      },
+      {
+        "label": "Timezone: ",
+        "text": "${widget.users[0]['location']['timezone']['description']}",
+      },
+    ];
+    //list to show only the searched data
+    _foundData = _allData;
+    super.initState();
+  }
+
+  // This function is called whenever the text field changes
+  void _runFilter(String enteredKeyword) {
+    List<Map<String, dynamic>> results = [];
+    if (enteredKeyword.isEmpty) {
+      // if the search field is empty or only contains white-space, we'll display all user data
+      results = _allData;
+    } else {
+      //contains only those field which start with entered letters
+      results = _allData
+          .where(
+            (data) => data["label"]
+                .toLowerCase()
+                .startsWith(enteredKeyword.toLowerCase(), 0),
+          )
+          .toList();
+      // we use the toLowerCase() method to make it case-insensitive
+    }
+    setState(() {
+      _foundData = results;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('User Profile')),
       body: ListView(padding: const EdgeInsets.all(10), children: <Widget>[
-        for (var user in widget.users)
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                alignment: Alignment.center,
-                child: Container(
-                  margin: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(54),
-                    border: Border.all(
-                      width: 5,
-                      color: const Color.fromARGB(255, 13, 174, 174),
-                    ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: Container(
+                margin: const EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(54),
+                  border: Border.all(
+                    width: 5,
+                    color: const Color.fromARGB(255, 13, 174, 174),
                   ),
-                  child: ClipOval(
-                    clipBehavior: Clip.antiAlias,
-                    child: Image.network(
-                      user['picture']['large'],
-                      height: 100,
-                      width: 100,
-                    ),
+                ),
+                child: ClipOval(
+                  clipBehavior: Clip.antiAlias,
+                  child: Image.network(
+                    widget.users[0]['picture']['large'],
+                    height: 100,
+                    width: 100,
                   ),
                 ),
               ),
-              formattedText(
-                "Name: ",
-                "${user['name']['title']}. ${user['name']['first']} ${user['name']['last']}",
+            ),
+            TextField(
+              onChanged: (value) => _runFilter(value),
+              decoration: const InputDecoration(
+                labelText: 'Search',
+                labelStyle: TextStyle(
+                  color: Color.fromARGB(255, 13, 174, 174),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+                suffixIcon: Icon(
+                  Icons.search,
+                  color: Color.fromARGB(255, 13, 174, 174),
+                ),
+                hintText: 'Enter which field to search',
+                hintStyle: TextStyle(
+                  color: Color.fromARGB(255, 13, 174, 174),
+                ),
               ),
-              formattedText(
-                "Username: ",
-                user['login']['username'],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            //if search returns null list
+            if (_foundData.isEmpty)
+              const Text(
+                "Sorry can't find the field",
+                style: TextStyle(
+                    fontSize: 24, color: Color.fromARGB(255, 164, 159, 158)),
               ),
-              formattedText(
-                "Email: ",
-                user['email'],
-              ),
-              formattedText(
-                "Phone: ",
-                user['phone'],
-              ),
-              formattedText(
-                "Cell: ",
-                user['cell'],
-              ),
-              formattedText(
-                "Gender: ",
-                user['gender'],
-              ),
-              formattedText(
-                "DOB: ",
-                user['dob']['date'].substring(0, 10),
-              ),
-              formattedText(
-                "Age: ",
-                user['dob']['age'].toString(),
-              ),
-              formattedText(
-                "Country: ",
-                user['location']['country'],
-              ),
-              formattedText(
-                "State: ",
-                user['location']['state'],
-              ),
-              formattedText(
-                "City: ",
-                user['location']['city'],
-              ),
-              formattedText(
-                "Street: ",
-                "${user['location']['street']['number']}, ${user['location']['street']['name']}",
-              ),
-              formattedText(
-                "Timezone: ",
-                user['location']['timezone']['description'],
-              ),
-            ],
-          )
+            //if search returns non-empty list
+            for (dynamic item in _foundData)
+              formattedText(item['label'], item['text']),
+          ],
+        )
       ]),
     );
   }
